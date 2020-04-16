246983e4e33aa71059f3dade42ee8a1e47a43550
antun <antun@antunkarlovac.com>
2020-04-16 06:36:34 -0700

    Topic 8: Download confirmed case data

--- bin/getCovidData.js
+++ bin/getCovidData.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var csv2json = require('csv2json');
 
 
-const csseCovid19TimeSeriesURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
+const timeSeriesDeathsUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
+const timeSeriesConfirmedCasesURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
 const countryLookupTableURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv';
 
 const downloadDir = './download';
@@ -12,7 +13,7 @@ if (!fs.existsSync(downloadDir)){
   fs.mkdirSync(downloadDir);
 }
 
-axios.get(csseCovid19TimeSeriesURL)
+axios.get(timeSeriesDeathsUrl)
   .then(function (response) {
     const rawOutputFile = downloadDir + '/' + 'time_series_covid19_deaths_global.csv';
     const jsonOutputFile = './src/data/' + 'time_series_covid19_deaths_global.json';
@@ -23,9 +24,22 @@ axios.get(csseCovid19TimeSeriesURL)
   })
   .catch(function (error) {
     // handle error
-    console.log('Error retrieving COVID-19 data from', csseCovid19TimeSeriesURL,  error);
+    console.log('Error retrieving COVID-19 data from', timeSeriesDeathsUrl,  error);
   });
 
+axios.get(timeSeriesConfirmedCasesURL)
+  .then(function (response) {
+    const rawOutputFile = downloadDir + '/' + 'time_series_covid19_confirmed_global.csv';
+    const jsonOutputFile = './src/data/' + 'time_series_covid19_confirmed_global.json';
+    fs.writeFileSync(rawOutputFile, response.data);
+    fs.createReadStream(rawOutputFile)
+    .pipe(csv2json())
+    .pipe(fs.createWriteStream(jsonOutputFile));
+  })
+  .catch(function (error) {
+    // handle error
+    console.log('Error retrieving COVID-19 data from', timeSeriesConfirmedCasesURL,  error);
+  });
 
 axios.get(countryLookupTableURL)
   .then(function (response) {
