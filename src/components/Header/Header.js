4b0c3aa4cae5d997383bbb37a2e8755668825423
antun <antun@antunkarlovac.com>
2020-04-21 21:58:49 -0700

    Tidied-up header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import classes from './Header.module.css';
+console.log('classes', classes);
+
 const header = () => (
-  <header className="App-header">
+  <header className={classes.Header}>
     <h1>COVID-19 Charts</h1>
-    Population-relative charts on the novel Coronavirus.
+    <p className={classes.tagline}>Population-relative charts on the novel Coronavirus.</p>
   </header>
 );
 
