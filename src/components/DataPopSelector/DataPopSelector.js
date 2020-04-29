e30ac27de62bb4786c3443abcee815d403c6f9ea
antun <antun@antunkarlovac.com>
2020-04-28 17:02:49 -0700

    Topic 14 / Support showing confirmed cases

--- src/components/DataPopSelector/DataPopSelector.js
+++ src/components/DataPopSelector/DataPopSelector.js
@@ -10,6 +10,22 @@ const dataPopSelector = (props) => {
 
   return (
     <div className={classes.PopulationSelector}>
+      <h3>Data to Show</h3>
+      <div className={classes.radioRow}>
+        <label>
+          <input type="radio" value="deaths"
+            checked={props.dataToShow==='deaths'}  onChange={props.onDataChanged}  />
+            Deaths
+        </label>
+      </div>
+      <div className={classes.radioRow}>
+        <label>
+          <input type="radio" value="confirmed"
+            checked={props.dataToShow==='confirmed'}  onChange={props.onDataChanged}  />
+          Confirmed Cases
+        </label>
+      </div>
+
       <h3>Relative to Population</h3>
       <Switch onChange={props.onSelect} checked={props.relative} />
       <div>
