f3998d1b0b3e30fee20c4532e3038fb0324e2901
antun <antun@antunkarlovac.com>
2020-04-28 17:43:55 -0700

    Show latest publish time in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import preval from 'preval.macro'
 
-const footer = () => (
-  <footer className="App-footer">
-    <div>
-      <a className="App-link" href="https://github.com/antun/covid-charts">covid-charts</a> v{process.env.REACT_APP_VERSION}
-    </div>
-  </footer>
-);
+const footer = () => {
+  const dateTimeStamp = preval`module.exports = new Date().toLocaleString();`
+  return (
+    <footer className="App-footer">
+      <div>
+        <a className="App-link" href="https://github.com/antun/covid-charts">covid-charts</a> v{process.env.REACT_APP_VERSION}
+        | updated: {dateTimeStamp}
+        | by <a className="App-link" href="http://www.antunkarlovac.com/">Antun Karlovac</a>
+      </div>
+    </footer>
+  )
+};
 
 export default footer;
